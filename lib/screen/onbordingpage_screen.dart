52a021d73a50d6c1e90ed8902e3f0556468e1e04
rasel-hossen <straselhossen24@gmail.com>
2022-06-04 00:49:51 +0600

    edi

--- lib/screen/onbordingpage_screen.dart
+++ lib/screen/onbordingpage_screen.dart
@@ -2,9 +2,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:job_finding_ui/screen/home.dart';
 
 class OnBordingPageScreen extends StatelessWidget {
   static const routeName = '/onbording-page';
+
+  DotsDecorator getDotDecoration() => DotsDecorator(
+        color: Color(0xFFBDBDBD),
+        activeColor: Colors.orange,
+        size: Size(10, 10),
+        activeSize: Size(22, 10),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) => SafeArea(
@@ -13,29 +24,29 @@ class OnBordingPageScreen extends StatelessWidget {
             PageViewModel(
               title: 'A reader lives a thousand lives',
               body: 'The man who never reads lives only one.',
-              footer: Text("data"),
-              image: Image.network(
-                  "https://img.freepik.com/free-vector/programmer-concept-illustration_114360-2417.jpg?w=740&t=st=1654274655~exp=1654275255~hmac=c90207ca7ee6b7b5c034aa9c498dc0fc5bc15705f063e3686ca2bff7d28d112b",),
+              image: Image.asset("assets/onBordingPageImage/ebook.png"),
             ),
             PageViewModel(
-              title: 'A reader lives a thousand lives',
-              body: 'The man who never reads lives only one.',
-              footer: Text("data"),
-              image: Image.network(
-                  "https://img.freepik.com/free-vector/programmer-concept-illustration_114360-2284.jpg?size=338&ext=jpg&ga=GA1.2.1205468974.1654265974"),
+              title: 'Featured Books',
+              body: 'Available right at your fingerprints',
+              image: Image.asset("assets/onBordingPageImage/readingbook.png")
             ),
             PageViewModel(
-              title: 'A reader lives a thousand lives',
-              body: 'The man who never reads lives only one.',
-              footer: Text("data"),
-              image: Image.network(
-                  "https://img.freepik.com/free-vector/programmer-concept-illustration_114360-3016.jpg?t=st=1654272454~exp=1654273054~hmac=997b50b01177a738cfd7adfd773a41ed26ac86f0e89919687bb80b9087147eb0&w=740"),
+              title: 'Simple UI',
+              body: 'For enhanced reading experience',
+              image: Image.asset("assets/onBordingPageImage/manthumbs.png"),
+            ),
+            PageViewModel(
+              title: 'Today a reader, tomorrow a leader',
+              body: 'Start your journey',
+              image: Image.asset("assets/onBordingPageImage/learn.png"),
             ),
           ],
           done: Text('NEXT', style: TextStyle(fontWeight: FontWeight.w600)),
-          onDone: () {},
+          onDone: () {Home();},
           showSkipButton: true,
           skip: Text('SKIP'),
+          dotsDecorator: getDotDecoration(),
           next: Icon(Icons.arrow_forward),
           globalBackgroundColor: Colors.white,
         ),
